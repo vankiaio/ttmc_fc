5c3bb5d5ea17f0d826cb753b43ff9e62d47e7bf3
vankiaio <liyi@vankia.cn>
2018-11-01 18:10:40 +0800

    v1.3.2 fix
    v1.3.2 fix

--- include/fc/static_variant.hpp
+++ include/fc/static_variant.hpp
@@ -218,7 +218,6 @@ public:
        );
        static const int value = impl::position<X, Types...>::pos;
     };
-
     static_variant()
     {
        _tag = 0;
@@ -230,15 +229,10 @@ public:
     {
        cpy.visit( impl::copy_construct<static_variant>(*this) );
     }
-
     static_variant( const static_variant& cpy )
     {
        cpy.visit( impl::copy_construct<static_variant>(*this) );
     }
-
-    static_variant( static_variant& cpy )
-    : static_variant( const_cast<const static_variant&>(cpy) )
-    {}
 
     static_variant( static_variant&& mv )
     {
@@ -347,10 +341,10 @@ public:
       try {
          _tag = w;
          impl::storage_ops<0, Types...>::con(_tag, storage);
-      } catch ( ... ) {
+      } catch ( ... ) { 
          _tag = 0;
          impl::storage_ops<0, Types...>::con(_tag, storage);
-      }
+      } 
     }
 
     int which() const {return _tag;}
@@ -370,7 +364,7 @@ struct visitor {
     typedef Result result_type;
 };
 
-   struct from_static_variant
+   struct from_static_variant 
    {
       variant& var;
       from_static_variant( variant& dv ):var(dv){}
@@ -390,7 +384,7 @@ struct visitor {
       typedef void result_type;
       template<typename T> void operator()( T& v )const
       {
-         from_variant( var, v );
+         from_variant( var, v ); 
       }
    };
 
